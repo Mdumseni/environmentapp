74947cf14dedb013958951d597a7b22e29fd4194
Mdumseni <95583263+Mdumseni@users.noreply.github.com>
2026-06-11 14:29:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -186,8 +186,12 @@ app.delete('/reports/:id', (req, res) => {
   });
 });
 
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
+});
+app.get('/', (req, res) => {
+  res.send('API is running');
 });
